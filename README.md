ec1ffa2771f275c8c0c4b5b8dcceea66af4e3eab
ahmedabdelkabier <ahmadabdulkabier55@yahoo.com>
2021-12-14 21:51:56 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# I am rich challenge app 
+- it's a flutter app from flutter course of # Bootcamp
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey[700],
+      appBar: AppBar(
+        title: Center(
+            child: Text('I AM RICH'),
+        ),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(image: AssetImage('images/55.png'),
+        ),
+      ),
+    ),
+   ),
+  );
+}
+
